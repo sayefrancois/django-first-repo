3d82313a2f0c6f03440acbd8350cbfe189e7ed83
Francois Saye <you@example.com>
2018-11-07 21:38:17 +0000

    first

---
+++ LoggingProject/LogApp/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render
+from LogApp.forms import UserForm, UserProfileInfoForm
+
+
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponseRedirect,HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+def index(request):
+    return render(request,'LogApp/index.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form= UserProfileInfoForm(data= request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user=user
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic=request.FILES['profile_pic']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request,'LogApp/registration.html',{'user_form':user_form,'profile_form':profile_form,'registered':registered})
+
+
+def user_login(request):
+    print('user_login def')
+
+    print('method == post')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    users = authenticate(username=username,password=password)
+
+    if users:
+        print('if user')
+
+        if users is not None:
+            print('log in succefull')
+
+            login(request,users)
+            print('log in succefull')
+            return HttpResponseRedirect(reverse('index'))
+            print('log in succefull')
+
+        else:
+            print('account not activ')
+
+            return HttpResponse('Acount not active')
+    else:
+        print('fail login')
+
+        # return HttpResponse('Some one tried to log in and fail')
+
+    return render(request,'LogApp/login.html',{})
